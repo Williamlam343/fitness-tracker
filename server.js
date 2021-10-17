bcc850c256a8cd56f625bb1953335a6d8134a912
Williamlam343 <87791763+Williamlam343@users.noreply.github.com>
2021-10-17 13:41:24 -0400

    server is spinnable/ need to fix routes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { clog } = require('./middleware/clog')
+const routes = require("./controller");
 
 
 
@@ -7,15 +9,13 @@ const PORT = process.env.PORT || 3000
 
 const app = express();
 
+app.use(clog)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static("public"))
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
-    useNewUrlParser: true,
-    useFindAndModify: false
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useUnifiedTopology: true });
 
 // routes
 app.use(routes);
