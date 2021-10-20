f60a4f64d75740c463cb0a69ea429583aef47df2
Williamlam343 <87791763+Williamlam343@users.noreply.github.com>
2021-10-19 21:31:45 -0400

    fixing routes

--- controller/api/workouts.js
+++ controller/api/workouts.js
@@ -1,5 +1,4 @@
 const router = require("express").Router()
-const { ObjectID, ObjectId } = require("bson")
 const Exercise = require("../../models")
 
 
@@ -52,18 +51,8 @@ router.get("/range", async (req, res) => {
 
 // creates an exercise
 router.post("/", async (req, res) => {
-    try {
-        res.json(
-
-            await Exercise.create(req.body)
-
-        )
-
-    } catch (error) {
-
-        res.json(error)
-
-    }
+    let data = await Exercise.create({})
+    res.json(data)
 
 })
 
@@ -72,39 +61,15 @@ router.put("/:id", async ({ body, params }, res) => {
     console.log(body)
     console.log(params.id)
     try {
-        let data = await Exercise.findByIdAndUpdate(
-            params.id,
-            { $push: { exercises: body } },
-            {
-                new: true
-            }
-        )
-        console.log(data)
+        let data = await Exercise.findByIdAndUpdate(params.id, { $push: { exercises: body } }, { new: true, runValidators: true })
+        res.json(data)
+
     } catch (error) {
         console.log(error)
         res.json(error)
     }
 })
 
-// router.put("/:id", ({ body, params }, res) => {
-//     Exercise.findOneAndUpdate(
-//         params.id,
-//         { $push: { exercises: body } },
-//         { new: true, runValidators: true })
-//         .then(workout_db => { res.json(workout_db); })
-//         .catch(err => { res.json(err); });
-// });
-
-
-// router.put("/:id", async (req, res) => {
-//     console.log(req.body)
-//     console.log(req.params.id)
-
-//     const filter = { _id: ObjectId(req.params.id) }
-//     const update = { $insert: { help: req.body } }
-//     let workoutData = await Exercise.findOneAndUpdate(filter, update, { new: true })
-//     console.log(workoutData)
-// })
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: true
 }
 );
 
